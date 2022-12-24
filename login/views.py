ca5913dce2751a5e8f77b73d949aab789fb56831
antonio waghtom <waghtompereira@gmail.com>
2022-12-23 23:30:56 -0300

    templates-login

--- login/views.py
+++ login/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def ENTRAR(request):
-    return HttpResponse('Pagina de login')
+    #return HttpResponse('Pagina de login')
+    return render(request, 'index.html')
