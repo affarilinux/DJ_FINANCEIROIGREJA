252a78873c9923e9c15455cff3500b0d4363ed51
antonio waghtom <waghtompereira@gmail.com>
2022-12-29 23:50:24 -0300

    templantes-pasta loguin

--- login/views.py
+++ login/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 def ENTRAR(request):
     #return HttpResponse('Pagina de login')
-    return render(request, 'index.html')
+    return render(request, 'login_t/index.html')
